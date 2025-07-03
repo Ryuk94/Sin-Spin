993f3d226bb8f9706a1120c20838dd48d528e827
Ryuk94 <jamesoseman@gmail.com>
2025-07-03 21:41:36 +0100

    Add files via upload

--- scenes/NewDayTransition.gd
+++ scenes/NewDayTransition.gd
@@ -1,11 +1,19 @@
 extends Control
 
 func _ready():
-	$CanvasLayer/NewDayDisplay.text = "[center][wave amp=10 freq=4]Day %d. The horrors persist and so does your debt[/wave][/center]" % Global.current_day
+	$CenterContainer/NewDayDisplay.text = "[center][wave amp=10 freq=5]Day %d. The horrors persist and so does your debt[/wave][/center]" % (Global.current_day + 1)
+	$AutoContinueTimer.start()
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.pressed:
+		_on_continue_button_pressed()
+
+func _on_AutoContinueTimer_timeout():
+	_on_continue_button_pressed()
 
 func _on_continue_button_pressed():
 	Global.winnings = 0
+	Global.current_day += 1
 	Global.used_escape_clause = false
 	Global.reset_upgrades()
-	
 	get_tree().change_scene_to_file("res://scenes/MainGame.tscn")
